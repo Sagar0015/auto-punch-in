73e98c269b25cdfa3f4aa33b91262a85b2154074
Sagar Giri <18695111+Sagar0015@users.noreply.github.com>
2025-02-14 11:08:15 +0545

    relogin and try to punch in in any failed cases

--- punchIn.py
+++ punchIn.py
@@ -63,32 +63,41 @@ try:
             else:
                 print("Punch-in failed!")
         except:
-            print("No punch-in pop up detected in optimis")
+            print("No punch-in pop up detected in optimis trying to relogin")
+
+             # # Re login again and repeat the process
+            driver.get('https://x.optimis.in/login')
+
+            # Log in
+            username = driver.find_element(By.ID, 'usrname')  # Replace with the actual ID or selector
+            password = driver.find_element(By.ID, 'passwd')  # Replace with the actual ID or selector
+            username.send_keys('sagar@savitriya.com')
+            password.send_keys('Sagar@12345')
+            password.send_keys(Keys.RETURN)
+
+            # Wait for the page to load
+            time.sleep(5)
+            print("X optimis logged in")
+            try:
+                punchInPopup =driver.find_element("xpath", "//h2[contains(text(), 'Would you like to start your day')]")
+            
+                # Navigate to the punch-in page
+                if punchInPopup:  # Replace with the actual ID or selector
+                    print("Punch-in pop up detected")
+                    punchInBtn1 =driver.find_element("xpath", "//button[contains(text(), 'Yes')]")
+                    punchInBtn1.click()
+                    print("Punch-in successful in x optimis!!")
+
+
+                else:
+                    print("Punch-in failed!")
+            except:
+                    print("Punch in failed!")
+
+
 
             # Open the HR management system
-        driver.get('https://hr.azminds.com/login')
-
-        # Log in
-        username = driver.find_element(By.ID, ':r0:')  # Replace with the actual ID or selector
-        password = driver.find_element(By.ID, ':r1:')  # Replace with the actual ID or selector
-        username.send_keys('sagar.azminds@gmail.com')
-        password.send_keys('azminds12345')
-        password.send_keys(Keys.RETURN)
-
-        # Wait for the page to load
-        time.sleep(5)
-        try:
-            azPunchInBtn =driver.find_element("xpath", "//button[contains(text(), 'Punch In')]")
-        
-            # Navigate to the punch-in page
-            if azPunchInBtn:  # Replace with the actual ID or selector
-                azPunchInBtn.click()
-                print("Punch-in completed successfully!",azPunchInBtn)
-                
-            else:
-                print("Punch-in failed!")
-        except:
-            print("No punch-in pop up detected")
+       
     else:
         
         print("Today is weekend!!")   
